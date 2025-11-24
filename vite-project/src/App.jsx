b3fba57cbd741ee472290ef1114e8c9377ecd813
Anuj Kumar <personeluse2000@gmail.com>
2025-11-24 17:06:36 +0530

    Initialize Vite project with React, add essential files and configurations
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling the application.
    - Implemented App.jsx as the main component with a simple counter.
    - Included React logo SVG in assets.
    - Established global styles in index.css.
    - Set up index.jsx for rendering the App component.
    - Configured main.jsx for React root rendering.
    - Added Vite configuration in vite.config.js.

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';  // ✅ Import useState
+
+function App() {
+  const [count, setCount] = useState(0);
+
+
+  return (
+  <div>
+    <h1>Chai aur React</h1>
+    <h2>Anuj welcome</h2>
+    <h1>Chai aur React</h1>
+  </div>
+)
+
+}
+
+export default App;
